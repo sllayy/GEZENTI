8cadd8ac5c3d44f658777e21c968b6ed740f1100
Mebalci <meminbalci06@gmail.com>
2025-08-26 09:35:19 +0300

    test

--- Dtos/RouteResponseDto.cs
+++ Dtos/RouteResponseDto.cs
@@ -4,7 +4,7 @@ namespace GeZenti.Api.Dtos
 {
     public class RouteResponseDto
     {
-        public double Distance { get; set; }       // metre
+        public double Distance { get; set; }       // metre TEST DEĞİŞİKLİK
         public double Duration { get; set; }       // saniye
         public JsonElement Geometry { get; set; }  // GeoJSON
         public List<int>? WaypointOrder { get; set; }
