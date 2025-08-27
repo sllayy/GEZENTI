bf9079bc19e9a44577c743b30f965223bb1f59e4
Mensure-Ogut <mensureogut@gmail.com>
2025-08-27 16:29:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,25 +20,25 @@ try
     Log.Information("Uygulama servisleri yapılandırılıyor...");
 
     // 2. Servisleri Ekleme (Dependency Injection)
-    
+
     // Veritabanı Bağlantısı
     builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
-    
+
     // HTTP İstekleri için
     builder.Services.AddHttpClient();
-    
+
     // API Controller'ları ve JSON Ayarları
     builder.Services.AddControllers().AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     });
-    
+
     // API Dokümantasyonu (Swagger)
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-    
+
     // Kendi yazdığımız servisler
     builder.Services.AddScoped<OsmService>();
     builder.Services.AddScoped<PoiService>();
@@ -46,4 +46,30 @@ try
     builder.Services.AddScoped<ICategoryService, CategoryService>();
     // Buraya diğer servisleriniz de (IRouteService vb.) eklenecek
 
-    // 3
+    // 3. HTTP Pipeline'ı Yapılandırma
+    var app = builder.Build();
+
+    // Gelen istekleri logla
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    Log.Information("Uygulama başlatılıyor...");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Uygulama başlatılırken ölümcül bir hata oluştu.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
